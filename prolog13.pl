07218658d5fb43a430de9db9823bf14ebc7cfedd
ryselis <djryse@gmail.com>
2014-05-30 01:32:02 +0300

    fixed indents

--- prolog13.pl
+++ prolog13.pl
@@ -1,5 +1,5 @@
-		can_divide(A, B) :- A mod B =:= 0.
-		can_divide(A, B) :- B * B < A, B1 is B + 1, can_divide(A, B1).
-		prime(2).
-		prime(3).
-		prime(X) :- not(can_divide(X, 2)).
+can_divide(A, B) :- A mod B =:= 0.
+can_divide(A, B) :- B * B < A, B1 is B + 1, can_divide(A, B1).
+prime(2).
+prime(3).
+prime(X) :- not(can_divide(X, 2)).
